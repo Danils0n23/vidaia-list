f2dbc4bc31bd45ea98c65e51e248d8ca5187707e
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-05-26 02:31:30 -0300

    update on projet about styles

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BsPencilSquare, BsTrash } from 'react-icons/bs'; // Importando os ícones do pacote react-icons/bs
+import { BsPencilSquare, BsTrash } from 'react-icons/bs';
+import { Link } from 'react-router-dom'; // Importe o Link do React Router
 
 const TaskContainer = styled.div`
   border: 1px solid #ddd;
@@ -8,15 +9,25 @@ const TaskContainer = styled.div`
   margin: 10px 0;
   display: flex;
   justify-content: space-between;
-  align-items: center; /* Centraliza os ícones verticalmente */
+  align-items: center;
 `;
 
 const IconsContainer = styled.div`
   display: flex;
-  gap: 10px; /* Espaçamento entre os ícones */
+  gap: 10px;
 `;
 
 const TaskItem = ({ task }) => {
+  const handleEditClick = () => {
+    // Lógica para lidar com o clique no ícone de edição
+    console.log('Editar tarefa:', task.id);
+  };
+
+  const handleDeleteClick = () => {
+    // Lógica para lidar com o clique no ícone de exclusão
+    console.log('Excluir tarefa:', task.id);
+  };
+
   return (
     <TaskContainer>
       <div>
@@ -26,10 +37,14 @@ const TaskItem = ({ task }) => {
         <p>{task.priority}</p>
       </div>
       <IconsContainer>
-        {/* Ícone de editar com tamanho e cor personalizados */}
-        <BsPencilSquare size={24} color="#007bff" />
-        {/* Ícone de excluir com tamanho e cor personalizados */}
-        <BsTrash size={24} color="#dc3545" />
+        {/* Link para a página de edição com o ícone de lápis */}
+        <Link to={`/edit/${task.id}`}>
+          <BsPencilSquare size={24} color="#007bff" onClick={handleEditClick} />
+        </Link>
+        {/* Link para a página de exclusão com o ícone de lixeira */}
+        <Link to={`/delete/${task.id}`}>
+          <BsTrash size={24} color="#dc3545" onClick={handleDeleteClick} />
+        </Link>
       </IconsContainer>
     </TaskContainer>
   );
